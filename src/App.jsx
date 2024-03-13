a70c94fd35d65746b30c49d44c4f337f983338b5
nayeem-miah <nayeem5113a@gmail.com>
2024-03-13 14:32:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/header/Header";
+
+function App() {
+  return <>
+  <Header>
+
+  </Header>
+  <Blogs></Blogs>
+  </>;
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">knowledge cafe </h1>
+    </div>
+  );
+};
+
+export default Header;
